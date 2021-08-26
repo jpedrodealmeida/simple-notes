d0beea5c6991c77d591369428c9d15525bd6174b
Joao Pedro <joao.sjc12@hotmail.com>
2021-08-26 17:05:58 -0300

    resolve module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './user/login/login.component';
+
+import { LoginComponent } from './user/components/login/login.component';
+
 
 const routes: Routes = [{
   path: '',
