7e0e19fd403f2e86fe797e9d4432d161d1652b37
Joao Pedro <joao.sjc12@hotmail.com>
2021-08-27 14:10:23 -0300

    create user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { 
+    
+  }
+
+  /**
+   * @author Joao Pedro
+   * @param user {name: string, password: string} 
+   * @description Method to save user in local storage
+   * @returns void
+   */
+  public saveUser(): void{
+
+  }
+}
