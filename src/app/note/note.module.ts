d5cf5085b21dfeba80eaaf1ae17394006ffd0ce0
Joao Pedro <joao.sjc12@hotmail.com>
2021-08-28 17:26:03 -0300

    create main menu

--- src/app/note/note.module.ts
+++ src/app/note/note.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
 import { NoteListComponent } from './components/note-list/note-list.component';
 import { MainComponent } from './components/main/main.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -11,7 +18,11 @@ import { MainComponent } from './components/main/main.component';
     MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ]
 })
 export class NoteModule { }
