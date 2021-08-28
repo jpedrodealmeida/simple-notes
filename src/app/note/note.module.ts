47589df19a5ee0f0d9211fe8009ec0dd1879c3d4
Joao Pedro <joao.sjc12@hotmail.com>
2021-08-28 17:47:14 -0300

    create note card component

--- src/app/note/note.module.ts
+++ src/app/note/note.module.ts
@@ -5,24 +5,28 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 
-
+import { MatCardModule } from '@angular/material/card';
 import { NoteListComponent } from './components/note-list/note-list.component';
 import { MainComponent } from './components/main/main.component';
 import { MatButtonModule } from '@angular/material/button';
+import { NoteCardComponent } from './components/note-card/note-card.component';
+
 
 
 
 @NgModule({
   declarations: [
     NoteListComponent,
-    MainComponent
+    MainComponent,
+    NoteCardComponent
   ],
   imports: [
     CommonModule,
     MatMenuModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class NoteModule { }
