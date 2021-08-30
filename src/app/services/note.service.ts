e179b178eb51c33246c507fce7efefbc9aafafc6
Joao Pedro <joao.sjc12@hotmail.com>
2021-08-30 18:14:33 -0300

    new format to save note

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NoteStorage } from '../interfaces/note-storage.interface';
 import { Note } from '../interfaces/note.interface';
+import { User } from '../interfaces/user.interface';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 
@@ -38,24 +39,40 @@ export class NoteService {
   private localStorageRegister(notes: Note[]){
     localStorage.setItem('notes', JSON.stringify(notes))
   }
-  private postNote(note: Note){
-    this.createNoteObjToRegister(note)
+  private postNote(note: Note): void{
     let notes = this.getNotes()
-      if (notes) {
-        note.id = notes.length
-        notes.push(note)
-      }
+      if (notes) 
+        this.updateNoteObjToRegister(notes, note)
       else
-        notes = [note]
-      this.localStorageRegister(notes)
+        notes = this.createNoteObjToRegister(note)
+    this.localStorageRegister(notes)
   }
-  private createNoteObjToRegister(noteToSave: Note){
+  private updateNoteObjToRegister(notesByUser: NoteStorage[], noteToSave: Note): NoteStorage[]{
+    let user: User = this.authService.getUserInformations()
+    let found = notesByUser.find(userNotes => userNotes.userId == user.id)
+    if (found) {
+      notesByUser.map((userNote: NoteStorage) => {
+        if (userNote.userId == user.id) {
+          userNote.notes.push(noteToSave)
+        }
+      });
+    } else {
+      let userNote: NoteStorage = {notes: [noteToSave], userId: user.id, shared: null}
+      notesByUser.push(userNote)
+    }
+    return notesByUser
+  }
+  private createNoteObjToRegister(noteToSave: Note): NoteStorage[]{
+    let userInfo = this.authService.getUserInformations()
     let users = this.userService.getUsers()
-    let note: NoteStorage[] = []
+    let noteList: NoteStorage[] = []
     users?.forEach(user => {
-      note.push({userId: user.id, notes: [noteToSave], shared: null})
+      if(user.id == userInfo.id)
+        noteList.push({userId: user.id, notes: [noteToSave], shared: null})
+      else
+        noteList.push({userId: user.id, notes: [], shared: null})
     })
-    debugger
+    return noteList
   }
   public getNotes(): any{
     let notes = localStorage.getItem('notes')
@@ -63,11 +80,17 @@ export class NoteService {
       return JSON.parse(notes)
     return notes
   }
-  public getNotesByUserId(userId: number){
-    let notes = this.getNotes()
-    if(notes)
-      notes = notes.filter((note: Note) => note.userId == userId)
-    return notes
+  public getNotesByUserId(userId: number): Note[]{
+    let notesByUser = this.getNotes()
+    let noteList!: Note[]
+    if(notesByUser){
+      notesByUser.forEach((userNote: NoteStorage) => {
+        if(userNote.userId == userId){
+          noteList = userNote.notes
+        }
+      });
+    }
+    return noteList
   }
   public getNoteById(noteId: number){
     let user = this.authService.getUserInformations()
@@ -75,10 +98,16 @@ export class NoteService {
     if(user){
       let notes = this.getNotesByUserId(user.id)
       if(notes)
-        note = notes.find((note: Note) => note.id == noteId)
+        note = this.findNote(notes, noteId)
     }
+    return note  
+  }
+  private findNote(notes: Note[], noteId: number): Note{
+    let note!: Note 
+    let found = notes.find((note: Note) => note.id == noteId)
+    if(found)
+        note = found
     return note
-    
   }
   public deleteNote(noteId: number){
     let notes = this.getNotes()
